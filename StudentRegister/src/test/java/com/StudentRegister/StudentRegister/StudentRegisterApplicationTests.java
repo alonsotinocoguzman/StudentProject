8423fc4dd97a86c4e3f45c88650274471f91f7bb
ALONS <juniortg.1189@gmail.com>
2024-05-18 12:42:03 -0500

    initial commit

---
+++ StudentRegister/src/test/java/com/StudentRegister/StudentRegister/StudentRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentRegister.StudentRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
